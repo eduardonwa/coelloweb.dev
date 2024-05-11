7e58f02c64fdc2ed3a8bdc63a68199e2f40df9c6
eduardonwa <eduardongua@hotmail.com>
2024-05-11 14:06:56 -0700

    hotmail updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,6 +55,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@hotmail.com') && $this->hasVerifiedEmail();
     }
 }
