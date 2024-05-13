e49736a4c485e9bd39bedcbc6a95cd98d7d81491
eduardonwa <eduardongua@hotmail.com>
2024-05-13 09:18:45 -0700

    bing site auth meta tag included

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="description" content="{{ $metaDescription }}">
+        <meta name="msvalidate.01" content="64EB583AF6E921E1270E6F29A784A037" />
         <title>{{ $metaTitle ?? 'Eduardo Coello' }}</title>
 
         <!-- Fonts -->
