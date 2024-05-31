59fea915ff20836c42f06e2df10390e046c0b1e1
eduardonwa <eduardongua@hotmail.com>
2024-05-31 12:20:42 -0700

    test

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,31 +10,36 @@ class BlogController extends Controller
     public function index(Post $post)
     {
         $lastPost = Post::with('category')
-            ->where('active', '=', 1)
-            ->take(1)
             ->latest()
+            ->take(1)
+            ->where('active', '=', 1)
             ->get();
 
         $secondLast = Post::with('category')
+            ->latest()
             ->where('active', '=', 1)
             ->take(1)
             ->skip(1)
             ->get();
 
-        $recentPosts = Post::where('active', '=', 1)
+        $recentPosts = Post::latest()
+            ->where('active', '=', 1)
             ->take(2)
             ->skip(2)
             ->get();
 
-        $popularPosts = Post::where('active', '=', 1)
+        $popularPosts = Post::latest()
+            ->where('active', '=', 1)
             ->take(2)
             ->skip(4)
+            ->with('category')
             ->get();
 
-        $morePosts = Post::with('category')
+        $morePosts = Post::latest()
             ->where('active', '=', 1)
             ->take(2)
             ->skip(6)
+            ->withCount('category')
             ->get();
 
         return view('blog', compact('lastPost', 'secondLast', 'recentPosts', 'popularPosts', 'morePosts'));
