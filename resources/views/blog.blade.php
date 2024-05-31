f6e258bc38cf23b72953fcc42d9f285bdfa44149
eduardonwa <eduardongua@hotmail.com>
2024-05-31 09:47:46 -0700

    test

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,17 +1,15 @@
 <x-blog-layout meta-description="¿Aún no tienes tu página web? ¡Yo te ayudo! Desde 500 pesos en adelante. Hablemos de ideas.">
-    <!-- blog posts -->
+
     <div class="overflow-hidden max-w-7xl mx-auto mt-8 pb-16 space-y-12 p-3 text-gray-900 dark:text-gray-100 lg:px-8 lg:grid lg:grid-cols-2">
-        <!-- contratame -->
         <div class="text-center lg:px-8 py-8 lg:col-span-2">
             <p class="text-lg pb-8 dark:text-woodsmoke-50 text-smoke">
-                Blog <span class="italic">(en proceso de ser completado)</span> de un diseñador gráfico apasionado por el desarrollo web.
-                Mi enfoque es aplicar todo lo que he aprendido desde que empecé a realizar proyectos web para empresarios como tú.
                 <span class="font-bold">Mi objetivo es crear interfaces web con significado.</span>
             </p>
             <a href="mailto:coelloweb@aol.com" class="outline-none focus:ring-4 focus:ring-monster-200 transition-colors ease-in-out duration-300 text-gray-50 bg-zinc-900 hover:bg-zinc-700 dark:hover:bg-monster-700 dark:bg-monster-600 p-4 text-lg rounded-xl font-semibold"
                 > Contrátame
             </a>
         </div>
+        <!-- contratame -->
 
         @if ($lastPost->isEmpty())
             <p class="text-woodsmoke-400 col-span-full text-center text-3xl">
@@ -47,29 +45,18 @@
                             sm:dark:bg-zinc-900 sm:w-4/6 lg:w-auto lg:h-auto lg:col-start-1 lg:row-start-2 hover:rotate-3 transition ease-in-out duration-150
                             {{ $loop->iteration % 2 == 0 ? 'md:self-end lg:self-auto' : 'even' }}"
                 >
-                    <div class="relative grid grid-rows-cards sm:flex sm:items-start sm:justify-center md:items-center sm:gap-x-4">
-                        <div class="z-0 p-4 pt-10 absolute h-auto w-full -top-5 row-start-2 border border-gray-300 dark:border-woodsmoke-800 rounded-md bg-white shadow-lg dark:bg-zinc-900
-                                    sm:relative sm:border-none sm:shadow-none sm:pl-4 sm:pt-4 sm:top-0 sm:rounded-none sm:bg-transparent md:p-2"
-                        >
-                            <a
-                                href="{{ route('posts.show', $recent->slug) }}"
-                                class="font-semibold text-xl text-woodsmoke-950 group-hover:text-monster-500 dark:text-monster-50 dark:group-hover:text-monster-400 transition ease-in-out duration-300"
-                            >
-                                {{ $recent->title }}
-                            </a>
-                            <p class="pt-2 text-base sm:pb-2 text-smoke dark:text-monster-50">
-                                {!! Str::limit($recent->caption, 140, '...') !!}
-                            </p>
-                        </div>
-                        <!-- card -->
+                    <div>
+                        <a href="">
+                            {{ $recent->title }}
+                        </a>
+                        <p>
+                            {!! Str::limit($recent->caption, 140, '...') !!}
+                        </p>
 
-                        <div class="row-start-1 z-10 h-full">
-                            <img
-                                src="{{ Storage::disk('public')->exists($recent->thumbnail) ? Storage::url($recent->thumbnail) : asset($recent->thumbnail) }}"
-                                alt="{{ $recent->title }}"
-                                class="container mx-auto w-72 h-full sm:w-s-233 rounded-md">
+                        <div>
+                            <img src="{{ Storage::disk('public')->exists($recent->thumbnail) ? Storage::url($recent->thumbnail) : asset($recent->thumbnail) }}" alt="">
                         </div>
-                        <!-- img -->
+
                     </div>
                 </div>
             @endforeach
@@ -133,10 +120,9 @@
             @endforeach
         </div>
         <!-- post 2 sin caption end -->
-
     </div>
+    <!-- blog posts -->
 
-    <!-- ad / service / newsletter -->
     <div class="h-s-377 space-y-12 bg-woodsmoke-300 p-4 flex justify-center flex-col items-center">
         <div class="space-y-3 flex justify-center flex-col items-center">
             <h1 class="text-5xl lg:text-6xl font-bold text-woodsmoke-950">
@@ -149,8 +135,8 @@
         <x-input-newsletter />
         <x-social-media-icons spacing="horizontal"/>
     </div>
+    <!-- ad / service / newsletter -->
 
-    <!-- more posts -->
     <div class="p-2 space-y-4 max-w-7xl mx-auto lg:px-8 my-16">
         <h1 class="px-2 text-woodsmoke-800 dark:text-woodsmoke-400 tracking-wider text-md">
             MÁS ENTRADAS
@@ -184,4 +170,5 @@
         @endforeach
 
     </div>
+    <!-- more posts -->
 </x-blog-layout>
