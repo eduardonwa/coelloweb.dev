d7cf8aa72c2566d290bc35bf800962f364127947
eduardonwa <eduardongua@hotmail.com>
2024-12-20 11:12:22 -0700

    probando navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,32 +2,42 @@
     <div class="site-header margin-inline-4">
         <div class="container" data-type="full-bleed">
             <div class="site-header__inner | padding-block-2">
-                <a href="{{ route('welcome') }}"><img src="/images/ecoello-logo.svg" alt=""></a>
+
+                <a class="site-header__logo" href="{{ route('welcome') }}">
+                    <img class="site-header__logo__wordmark" src="/images/ecoello-logo.svg" alt="">
+                    {{-- <img class="site-header__logo__icon" src="/images/ecoello-icon.svg" alt=""> --}}
+                </a>
+
                 <nav aria-label="primary navigation">
                     <ul class="nav | flex-group">
                         <li>
                             <a href="{{ route('welcome') }}">
-                                Inicio
+                                <span>Inicio</span>
+                                <img src="/images/navbar-home-icon.png" alt="">
                             </a>
                         </li>
                         <li>
                             <a href="{{ route('acerca') }}">
-                                Acerca
+                                <span>Acerca</span>
+                                <img src="/images/navbar-acerca-icon.png" alt="">
                             </a>
                         </li>
                         <li>
                             <a href="{{ route('servicios') }}">
-                                Servicios
+                                <span>Servicios</span>
+                                <img src="/images/navbar-servicios-icon.png" alt="">
                             </a>
                         </li>
                         <li>
                             <a href="{{ route('blog.index') }}">
-                                Blog
+                                <span>Blog</span>
+                                <img src="/images/navbar-blog-icon.png" alt="">
                             </a>
                         </li>
                         <li>
                             <a href="{{ route('contacto') }}">
-                                Contacto
+                                <span>Contacto</span>
+                                <img src="/images/navbar-contacto-icon.png" alt="">
                             </a>
                         </li>
                     </ul>
