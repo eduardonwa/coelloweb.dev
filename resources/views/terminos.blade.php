4f4b9e43508c1252190973085da5037cd04ef076
eduardonwa <eduardongua@hotmail.com>
2024-12-24 19:05:26 -0700

    pagina privacidad y terminos

--- resources/views/terminos.blade.php
+++ resources/views/terminos.blade.php
@@ -1,12 +1,12 @@
 <x-site-layout>
 
-    <section class="terminos-wrap">
-        <h1 style="font-weight: 700;">TÉRMINOS Y CONDICIONES</h1>
+    <section class="container padding-block-10">
+        <h1 class="fs-700 uppercase">TÉRMINOS Y CONDICIONES</h1>
             El presente contrato describe los términos y condiciones aplicables al uso del contenido, productos y/o servicios del sitio web
             CoelloWeb del cual es titular Eduardo Robles Coello. Para hacer uso del contenido, productos y/o servicios del sitio web el usuario
             deberá sujetarse a los presentes términos y condiciones.
         <br><br>
-        <h2 style="font-weight: 700;">I. OBJETO</h2>
+        <h2 class="fs-700 uppercase">I. Objeto</h2>
             <li>El objeto es regular el acceso y utilización del contenido, productos y/o servicios a disposición del público en general en el dominio <a href="https://www.coelloweb.dev">https://www.coelloweb.dev</a>.</li>
             <li>El titular se reserva el derecho de realizar cualquier tipo de modificación en el sitio web en cualquier momento y sin previo aviso, el usuario acepta dichas modificaciones.</li>
             <li>El acceso al sitio web por parte del usuario es libre y gratuito, la utilización del contenido, productos y/o servicios implica un costo de suscripción para el usuario.</li>
@@ -14,32 +14,32 @@
             <li>El sitio web está dirigido a usuarios residentes en México y cumple con la legislación establecida en dicho país, si el usuario reside en otro país y decide acceder al sitio web lo hará bajo su responsabilidad.</li>
             <li>La administración del sitio web puede ejercerse por terceros, es decir, personas distintas al titular, sin afectar esto los presentes términos y condiciones.</li>
         <br><br>
-        <h2 style="font-weight: 700;">II. USUARIO</h2>
+        <h2 class="fs-700 uppercase">II. Usuario</h2>
             <li>La actividad del usuario en el sitio web como publicaciones o comentarios estarán sujetos a los presentes términos y condiciones.</li>
             <li>El usuario se compromete a utilizar el contenido, productos y/o servicios de forma lícita, sin faltar a la moral o al orden público, absteniéndose de realizar cualquier acto que afecte los derechos de terceros o el funcionamiento del sitio web.</li>
             <li>El usuario se compromete a proporcionar información verídica en los formularios del sitio web.</li>
             <li>El acceso al sitio web no supone una relación entre el usuario y el titular del sitio web.</li>
             <li>El usuario manifiesta ser mayor de edad y contar con la capacidad jurídica de acatar los presentes términos y condiciones.</li>
         <br><br>
-        <h2 style="font-weight: 700;">III. ACCESO Y NAVEGACIÓN EN EL SITIO WEB</h2>
+        <h2 class="fs-700 uppercase">III. Acceso y navegación en el sitio web</h2>
             <li>El titular no garantiza la continuidad y disponibilidad del contenido, productos y/o servicios en el sitito web, realizará acciones que fomenten el buen funcionamiento de dicho sitio web sin responsabilidad alguna.</li>
             <li>El titular no se responsabiliza de que el software esté libre de errores que puedan causar un daño al software y/o hardware del equipo del cual el usuario accede al sitio web. De igual forma, no se responsabiliza por los daños causados por el acceso y/o utilización del sitio web.</li>
         <br><br>
-        <h2 style="font-weight: 700;">IV. POLÍTICA DE PRIVACIDAD Y PROTECCIÓN DE DATOS</h2>
+        <h2 class="fs-700 uppercase">IV. Política de privacidad y protección de datos</h2>
             <li>Conforme a lo establecido en la Ley Federal de Protección de Datos Personales en Posesión de Particulares, el titular se compromete a tomar las medidas necesarias que garanticen la seguridad del usuario, evitando que se haga uso indebido de los datos personales que el usuario proporcione en el sitio web.</li>
             <li>El titular corroborará que los datos personales contenidos en las bases de datos sean correctos, verídicos y actuales, así como que se utilicen únicamente con el fin con el que fueron recabad</li>
             <li>El uso de datos personales se limitará a lo previsto en el Aviso de Privacidad disponible en el <a href="https://www.coelloweb.dev/privacidad">aviso de privacidad</a>.</li>
             <li>EduardoCoello se reserva el derecho de realizar cualquier tipo de modificación en el Aviso de Privacidad en cualquier momento y sin previo aviso, de acuerdo con sus necesidades o cambios en la legislación aplicable, el usuario acepta dichas modificaciones.</li>
             <li>El sitio web implica la utilización de cookies que son pequeñas cantidades de información que se almacenan en el navegador utilizado por el usuario como datos de ingreso, preferencias del usuario, fecha y hora en que se accede al sitio web, sitios visitados y dirección IP, esta información es anónima y solo se utilizará para mejorar el sitio web. Los cookies facilitan la navegación y la hacen más amigable, sin embargo, el usuario puede desactivarlos en cualquier momento desde su navegador en el entendido de que esto puede afectar algunas funciones del sitio web.</li>
         <br><br>
-        <h2 style="font-weight: 700;">V. POLÍTICA DE ENLACES</h2>
+        <h2 class="fs-700 uppercase">V. Política de enlaces</h2>
             <li>En el sitio web puede contener enlaces a otros sitios de internet pertenecientes a terceros de los cuales no se hace responsable.</li>
         <br><br>
-        <h2 style="font-weight: 700;">VI. POLÍTICA DE PROPIEDAD INTELECTUAL E INDUSTRIAL</h2>
+        <h2 class="fs-700 uppercase">VI. Política de propiedad intelectual e industrial</h2>
             <li>El titular manifiesta tener los derechos de propiedad intelectual e industrial del sitio web incluyendo imágenes, archivos de audio o video, logotipos, marcas, colores, estructuras, tipografías, diseños y demás elementos que lo distinguen, protegidos por la legislación mexicana e internacional en materia de propiedad intelectual e industrial.</li>
             <li>El usuario se compromete a respetar los derechos de propiedad industrial e intelectual del titular pudiendo visualizar los elementos del sitio web, almacenarlos, copiarlos e imprimirlos exclusivamente para uso personal.</li>
         <br><br>
-        <h2 style="font-weight: 700;">VII. LEGISLACIÓN Y JURISDICCIÓN APLICABLE</h2>
+        <h2 class="fs-700 uppercase">VII. Legislación y jurisdicción aplicable</h2>
             <li>La relación entre el usuario y el titular se regirá por las legislaciones aplicables en México.</li>
             <li>EduardoCoello no se responsabiliza por la indebida utilización del contenido, productos y/o servicios del sitio web y del incumplimiento de los presentes términos y condiciones.</li>
     </section>
