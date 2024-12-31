782ae9f9a4b54265f15da9c1c0e6ac3bb363d678
eduardonwa <eduardongua@hotmail.com>
2024-12-31 10:26:29 -0700

    meta tags para layouts/site.blade.php

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html lang="es-ES">
 <head>
-    <!-- open graph -->
+    <meta charset="UTF-8">
     <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:description" content="{{ $metaDescription }}">
+    <meta property="og:description" content="{{ $metaDescription ?? 'Diseño web profesional para mejorar tu presencia online.' }}">
     <meta property="og:locale" content="es_ES">
     <meta property="og:site_name" content="Eduardo Coello | Diseño Web">
-    {{-- <meta property="og:image:secure_url" content="{{ asset('images/eduardocoello-foto.png') }}">
+    <meta property="og:image" content="{{ asset('images/ecoello-logo.png') }}">
+    <meta property="og:image:secure_url" content="{{ asset('images/ecoello-logo.png') }}">
     <meta property="og:image:type" content="image/png">
     <meta property="og:image:width" content="320">
-    <meta property="og:image:height" content="301"> --}}
-    <meta charset="UTF-8">
-    <meta name="image" property="og:image" content="{{ asset('images/ecoello-logo.png') }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:image:height" content="301">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="{{ $metaTitle ?? 'Eduardo Coello | Diseña tu web' }}">
     <title>{{ $metaTitle ?? 'Eduardo Coello - Diseña tu web' }}</title>
     @vite(['resources/js/app.js', 'resources/js/animaciones.js'])
     @livewireStyles
