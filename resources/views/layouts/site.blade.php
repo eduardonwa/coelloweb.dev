2f895c3c711a0a5daf92279638347d95fb0633b9
eduardonwa <eduardongua@hotmail.com>
2024-12-31 10:10:54 -0700

    og:image

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -11,6 +11,7 @@
     <meta property="og:image:width" content="320">
     <meta property="og:image:height" content="301">
     <meta charset="UTF-8">
+    <meta name="image" property="og:image" content="{{ asset('images/eduardocoello-foto.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $metaTitle ?? 'Eduardo Coello - Diseña tu web' }}</title>
