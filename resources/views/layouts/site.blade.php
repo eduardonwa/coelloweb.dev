8e0373fff1caaf43bc69ea4fdbb084df14b258b9
eduardonwa <eduardongua@hotmail.com>
2024-12-31 10:14:52 -0700

    ecoello-logo en og:image

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -6,12 +6,12 @@
     <meta property="og:description" content="{{ $metaDescription }}">
     <meta property="og:locale" content="es_ES">
     <meta property="og:site_name" content="Eduardo Coello | Diseño Web">
-    <meta property="og:image:secure_url" content="{{ asset('images/eduardocoello-foto.png') }}">
+    {{-- <meta property="og:image:secure_url" content="{{ asset('images/eduardocoello-foto.png') }}">
     <meta property="og:image:type" content="image/png">
     <meta property="og:image:width" content="320">
-    <meta property="og:image:height" content="301">
+    <meta property="og:image:height" content="301"> --}}
     <meta charset="UTF-8">
-    <meta name="image" property="og:image" content="{{ asset('images/eduardocoello-foto.png') }}">
+    <meta name="image" property="og:image" content="{{ asset('images/ecoello-logo.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $metaTitle ?? 'Eduardo Coello - Diseña tu web' }}</title>
