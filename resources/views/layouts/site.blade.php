2897934a1e975b7efe6e96f2cf49cd108e31d967
eduardonwa <eduardongua@hotmail.com>
2025-01-02 11:22:59 -0700

    orden en las meta tags

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -2,21 +2,24 @@
 <html lang="es-ES">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- open graph -->
     <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:description" content="{{ $metaDescription ?? 'Diseño web profesional para mejorar tu presencia online.' }}">
-    <meta property="og:locale" content="es_ES">
+    <meta property="og:site_name" content="Eduardo Coello | Diseño Web">
+    <meta property="og:description" content="{{ $metaDescription ?? 'Diseño web enfocado en las personas. Para que tu sitio web te encante a ti, y a los tuyos.'}}">
+    <meta property="og:locale" content="es_MX">
     <meta property="og:site_name" content="Eduardo Coello | Diseño Web">
     <meta property="og:image" content="{{ asset('images/ecoello-logo.png') }}">
-    <meta property="og:image:secure_url" content="{{ asset('images/ecoello-logo.png') }}">
-    <meta property="og:image:type" content="image/png">
     <meta property="og:image:width" content="358">
     <meta property="og:image:height" content="252">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="{{ $metaTitle ?? 'Eduardo Coello | Diseña tu web' }}">
-    <meta name="author" content="Eduardo Coello">
+    <!-- titulo -->
     <title>{{ $metaTitle ?? 'Eduardo Coello - Diseña tu web' }}</title>
+    <!-- recursos vite -->
     @vite(['resources/js/app.js', 'resources/js/animaciones.js'])
     @livewireStyles
+    <!-- scripts externos -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/17.6.1/lazyload.min.js"></script>
     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
     <style>
