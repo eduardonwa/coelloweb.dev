a5274f236aaf457e4839582b385ec3ca90d3099d
Eduardo Coello <eduardongua@hotmail.com>
2025-12-17 11:43:07 -0700

    subdominio creado con una vista

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::domain('links.' . env('APP_URL'))
+            ->middleware('web')
+            ->group(base_path('routes/links.php'));
     }
 }
